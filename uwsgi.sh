17536fe2eb774d3802f7041b7c6c90d006dcc324
ashtneoi <ashtneoi@gmail.com>
2018-03-14 23:08:54 -0700

    Shift args so DIR isn't the first ARG

--- uwsgi.sh
+++ uwsgi.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -eu
 
-if [[ $# != 1 ]]; then
-    echo "Usage: $0 DIR" >&2
+if [[ $# < 1 ]]; then
+    echo "Usage: $0 DIR [ARG ...]" >&2
     exit 2
 fi
 
-source .env/bin/activate && uwsgi --yaml "$1/uwsgi.yaml" "$@"
+DIR=$1
+shift
+source .env/bin/activate && uwsgi --yaml "$DIR/uwsgi.yaml" "$@"
