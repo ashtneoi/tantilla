b0ab259fcc508ec373d6727572da3f4131511534
ashtneoi <ashtneoi@gmail.com>
2018-04-09 23:44:31 -0700

    Refactor slightly

--- tantilla.py
+++ tantilla.py
@@ -13,17 +13,18 @@ def create_app(mount_point, url_map):
         [Rule(mount_point + path, endpoint=ep) for (path, ep) in url_map]
     )
     def app(environ, start_response):
-        try:
-            endpoint, values = url_map.bind_to_environ(environ).match()
-            with Request(environ) as req:
+        with Request(environ) as req:
+            try:
+                endpoint, values = url_map.bind_to_environ(environ).match()
                 if req.method not in ('GET', 'POST'):
                     return abort(400)(environ, start_response)
-                return endpoint(req)(environ, start_response)
-        except NotFound:
-            with open("special/404.html") as f:
-                return HTMLResponse(
-                    f.read(), status=404,
-                )(environ, start_response)
-        except HTTPException as e:
-            return e(environ, start_response)
+                response = endpoint(req)
+            except NotFound:
+                with open("special/404.html") as f:
+                    response = HTMLResponse(
+                        f.read(), status=404,
+                    )
+            except HTTPException as e:
+                response = e
+            return response(environ, start_response)
     return app
