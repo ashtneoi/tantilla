531b239bc59a1091fc8acc3961595677cfc71185
ashtneoi <ashtneoi@gmail.com>
2018-04-13 01:38:30 -0700

    Handle BadRequestKeyError better; improve error pages

--- tantilla.py
+++ tantilla.py
@@ -1,6 +1,8 @@
 from functools import partial
+from traceback import print_exc
 
-from werkzeug.exceptions import abort, HTTPException, NotFound
+from werkzeug.exceptions import \
+    abort, BadRequestKeyError, HTTPException, NotFound
 from werkzeug.routing import Map, Rule
 from werkzeug.wrappers import Request, Response
 
@@ -8,14 +10,23 @@ from werkzeug.wrappers import Request, Response
 HTMLResponse = partial(Response, content_type='text/html')
 
 
-def default_not_found(req):
-    with open("special/404.html") as f:
-        return HTMLResponse(
-            f.read(), status=404,
-        )
+def status(req, code):
+    if code == 404:
+        with open("special/404.html") as f:
+            return HTMLResponse(
+                f.read(), status=404,
+            )
+    elif 500 <= code < 600:
+        with open("special/50x.html") as f:
+            return HTMLResponse(
+                f.read(), status=404,
+            )
+    else:
+        print("warning: unhandled status code {}".format(code))
+        abort(code)
 
 
-def create_app(mount_point, url_map, not_found=default_not_found):
+def create_app(mount_point, url_map, status=status):
     url_map = Map(
         [Rule(mount_point + path, endpoint=ep) for (path, ep) in url_map]
     )
@@ -23,12 +34,17 @@ def create_app(mount_point, url_map, not_found=default_not_found):
         with Request(environ) as req:
             try:
                 endpoint, values = url_map.bind_to_environ(environ).match()
-                if req.method not in ('GET', 'POST'):
-                    return abort(400)(environ, start_response)
-                response = endpoint(req, **values)
+                if req.method in ('GET', 'POST'):
+                    resp = endpoint(req, **values)
+                else:
+                    resp = status(req, 400)(environ, start_response)
             except NotFound:
-                response = not_found(req)
+                resp = status(req, 404)
+            except BadRequestKeyError as e:
+                # raised when indexing req.args or req.form fails
+                print_exc()
+                resp = status(req, 500)
             except HTTPException as e:
-                response = e
-            return response(environ, start_response)
+                resp = e
+            return resp(environ, start_response)
     return app
